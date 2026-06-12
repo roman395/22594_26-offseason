e03b2aa088f514c4a6cb8f064d20289fa6ad11b8
roman395 <goruskinroma@gmail.com>
2026-06-12 22:45:57 +0300

    add Drivetrain and DrivetrainConfig

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/DrivetrainConfig.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.configurations;
+
+import com.pedropathing.geometry.Pose;
+
+public class DrivetrainConfig {
+  public static Pose RED_START_POSE = new Pose(0,0,0);
+  public static Pose BLUE_START_POSE = new Pose(0,0,0);
+  public static double TURN_SLOWER_COF = 0.65;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Drivetrain.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.configurations.DrivetrainConfig;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.utils.Alliance;
+import org.firstinspires.ftc.teamcode.utils.MultipleTelemetry;
+
+public class Drivetrain extends Module {
+  private Follower follower;
+  Alliance alliance;
+  
+  public Drivetrain(OpMode opMode, MultipleTelemetry telemetry, Alliance alliance) {
+    super(opMode, telemetry);
+    this.alliance = alliance;
+  }
+  
+  @Override
+  public void init() {
+    follower = Constants.createFollower(opMode.hardwareMap);
+    follower.setStartingPose(alliance == Alliance.RED ? DrivetrainConfig.RED_START_POSE :
+        DrivetrainConfig.BLUE_START_POSE);
+  }
+  
+  @Override
+  public void start() {
+    follower.startTeleOpDrive();
+  }
+  
+  @Override
+  public void loop() {
+    follower.update();
+    follower.setTeleOpDrive(
+        opMode.gamepad1.left_stick_y,
+        opMode.gamepad1.left_stick_x,
+        (opMode.gamepad1.right_trigger - opMode.gamepad1.left_trigger) * DrivetrainConfig.TURN_SLOWER_COF,
+        false
+    );
+  }
+  
+  @Override
+  public void addTelemetry() {
+  
+  }
+  
+}
