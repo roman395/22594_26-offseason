d138ae2477271d3cdeac390b8d6dac257584a291
roman395 <goruskinroma@gmail.com>
2026-06-12 23:43:18 +0300

    add HardwareNames config

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurations/HardwareNames.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.configurations;
+
+public class HardwareNames {
+  public static String frontLeftMotor = "FLM";
+  public static String rearLeftMotor = "RLM";
+  public static String frontRightMotor = "FRM";
+  public static String rearRightMotor = "RLM";
+  public static String forwardOdometry = "";//TODO fill
+  public static String strafeOdometry = "";//TODO fill
+  public static String imu = "imu";
+  
+
+}
